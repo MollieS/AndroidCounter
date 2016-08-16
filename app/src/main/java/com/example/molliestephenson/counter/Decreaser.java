ae5d5df4cd2895306ef7a9fb02c7e7c33696bf46
Mollie Stephenson <mollie@8thlight.com>
2016-08-16 15:55:18 +0100

    Adds expresso testing for acceptance tests

---
+++ app/src/main/java/com/example/molliestephenson/counter/Decreaser.java
@@ -0,0 +1,8 @@
+package com.example.molliestephenson.counter;
+
+public class Decreaser implements NumberChanger {
+
+    public int changeNumber(int number) {
+        return --number;
+    }
+}
--- app/src/main/java/com/example/molliestephenson/counter/MainActivity.java
+++ app/src/main/java/com/example/molliestephenson/counter/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.molliestephenson.counter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mUpButton;
+    private Button mDownButton;
+    private TextView mNumberDisplay;
+    private int mNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNumberDisplay = (TextView) findViewById(R.id.textView);
+        mNumber = 0;
+        setUpButton();
+        setDownButton();
+    }
+
+    private void setDownButton() {
+        mDownButton = (Button) findViewById(R.id.down);
+        mDownButton.setOnClickListener(new NumberViewChange(new Decreaser()));
+    }
+
+    private void setUpButton() {
+        mUpButton = (Button) findViewById(R.id.up);
+        mUpButton.setOnClickListener(new NumberViewChange(new Increaser()));
+    }
+
+    private class NumberViewChange implements View.OnClickListener {
+
+        private NumberChanger numberChanger;
+
+        public NumberViewChange(NumberChanger numberChanger) {
+            this.numberChanger = numberChanger;
+        }
+
+        @Override
+        public void onClick(View view) {
+            mNumber = numberChanger.changeNumber(mNumber);
+            mNumberDisplay.setText(String.valueOf(mNumber));
+        }
     }
 }
